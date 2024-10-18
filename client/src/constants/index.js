51a5f9472d643400441f88b49924f47dfd6d14d9
hasanasadov <hasanaliasadov@gmail.com>
2024-10-19 00:54:26 +0400

    adding server

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,7 +1,7 @@
 export const PASSWORD_REGEX =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://hasauth-server.vercel.app";
 
 export const USER_ROLE = {
   ADMIN: "Admin",
